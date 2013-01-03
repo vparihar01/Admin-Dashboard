f172ad71d4ab7616e3f65edc3c154b2e414e7d14
Vivek Parihar <vivek@weboniselab.com>
2013-01-03 16:53:30 +0530

    comments added

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -6,7 +6,7 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "Authuser"
-  config.before_filter :check_admin_role
+  config.before_filter :check_admin_role # check if user is admin or not
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
