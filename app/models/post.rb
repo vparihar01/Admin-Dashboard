6f587c412b694d174ef6093827e57809db8c8c73
Bhushan <bhushan@weboniselab.com>
2013-01-04 14:23:20 +0530

    working on rspec

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
 
+  #validates :title, :presence => true
   #NOTE Return the recently published visible post ordered by their publish date
   scope :recent, limit(10).order("created_at ASC")
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -22,17 +22,47 @@ describe PostsController do
     it "should show the new post page " do
       @ability.can :create, Post
       get :new
-      response.code.should == "200"
+      assert_template :new
     end
   end
 
   describe 'GET edit' do
     it "should show the edit post page " do
-      @ability.can :create, Post
+      @ability.can :update, Post
       @post = Factory(:post, :user_id => @user.id)
       get :edit, :id => @post.id
+      assert_template :edit
     end
   end
+
+  describe 'POST create' do
+    context "with valid attributes" do
+      it "should create a new post" do
+        expect{
+          post "create_offer", :offer => Factory.attributes_for(:offer)
+        }.to change(Offer,:count).by(1)
+      end
+
+      it "redirects to the Offers list page" do
+        post "create_offer", :offer => Factory.attributes_for(:offer)
+        response.should redirect_to "/offers"
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not creates a new offer" do
+        expect{
+          post "create_offer", :offer => Factory.attributes_for(:invalid_offer)
+        }.not_to change(Offer,:count)
+      end
+
+      it "re-renders to the create new Offer form" do
+        post "create_offer", :offer => Factory.attributes_for(:invalid_offer)
+        response.should render_template :new
+      end
+    end
+  end
+
 
 
 end
